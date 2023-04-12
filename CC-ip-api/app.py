6d2bfe7ba705758fcce7a7347cf99a5798def7b6
Fredrik Welander <welandfr@arcada.fi>
2023-04-12 08:54:18 +0000

    lektion 3 api

--- CC-ip-api/app.py
+++ CC-ip-api/app.py
@@ -6,7 +6,13 @@ CORS(app) # Tillåt cross-origin requests
 
 @app.route("/")
 def hello():
-    return request.remote_addr
+    # i Flask kan vi skicka dict direkt, blir JSON automatiskt
+    return { 
+        "ip": request.remote_addr,
+        "msg": "hello!",
+        "lista": [1, 2, 3, "hej"], # list
+        "octets": request.remote_addr.split(".") #list
+    }
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
